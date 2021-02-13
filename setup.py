babf6013c89ecf3d06a084b7738331d6a217fbc0
dlopezpa <dlpadial@opensistemas.com>
2021-02-13 18:23:08 +0100

    level up version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scoringhandlerutils',
-      version='0.0.1',
+      version='0.0.2',
       description='Utils functions for scoring handler',
       url='https://github.com/bukosabino/scoring-handler-utils',
       author='Dario Lopez Padial (Bukosabino)',
