071288410d09c1c5be2e215d05125361d06b5146
dlopezpa <dlpadial@opensistemas.com>
2021-02-14 19:15:28 +0100

    type annotations

--- api_model_utils/app.py
+++ api_model_utils/app.py
@@ -23,7 +23,7 @@ class ManagerProfile:
         return parser
 
     @staticmethod
-    def _prepare_output_path(path_profile, filename):
+    def _prepare_output_path(path_profile: str, filename: str):
         if not os.path.exists(path_profile):
             path_profile = os.path.join(os.getcwd(), "output")
 
@@ -45,20 +45,18 @@ class ManagerProfilePyinstrument(ManagerProfile):
 
     def stop_and_write(self, path_profile: str, is_docker: bool, sync: bool):
         self.profiler.stop()
-
         filename = (
             "pyinstrument_profile_sync.html"
             if sync
             else "pyinstrument_profile_async.html"
         )
-
         if not is_docker:
             output_html = self.profiler.output_html()
             self._write_output_file(path_profile, output_html, filename=filename)
 
         print(self.profiler.output_text(unicode=True, color=True))
 
-    def _write_output_file(self, path_profile, output_html, filename):
+    def _write_output_file(self, path_profile: str, output_html: str, filename: str):
         output_html_path = self._prepare_output_path(path_profile, filename)
         with open(output_html_path, "w") as file:
             file.write(output_html)
@@ -71,14 +69,14 @@ class ManagerProfileYappi(ManagerProfile):
         context_manager = yappi.run()
         return context_manager
 
-    def stop_and_write(self, path_profile, is_docker: bool, sync: bool):
+    def stop_and_write(self, path_profile: str, is_docker: bool, sync: bool):
         stats = yappi.get_func_stats()
         filename = "yappi_profile_sync.html" if sync else "yappi_profile_async.html"
         if not is_docker:
             self._write_output_file(path_profile, stats, filename=filename)
         stats.print_all()
 
-    def _write_output_file(self, path_profile, stats, filename):
+    def _write_output_file(self, path_profile: str, stats, filename: str):
         output_txt_path = self._prepare_output_path(path_profile, filename)
         with open(output_txt_path, "w") as file:
             stats.print_all(out=file)
